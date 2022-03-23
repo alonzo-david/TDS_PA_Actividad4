ccefa3b41fc7580d81f2fba221b2c497a3be2346
david-alonzo <dajualonzo@gmail.com>
2022-03-22 21:58:47 -0600

    Renderer
    Optimize fetch method, add a height and width buttons

--- renderer.js
+++ renderer.js
@@ -1,47 +1,55 @@
 //window.comunicacion.registroValido('OK')
 
-window.comunicacion.inicioCorrecto(function(event, args){
+window.comunicacion.inicioCorrecto(function (event, args) {
     console.log(args)
 })
 
 let boton_solicitud = document.getElementById('consultar')
-let input_tipo = document.getElementById('tipo')
+
+
 let input_filtro = document.getElementById('filtro')
+let input_alto = document.getElementById('alto')
+let input_ancho = document.getElementById('ancho')
+
+
 let imagen_respuesta = document.getElementById('respuesta')
+
+
 boton_solicitud.addEventListener('click', realizarSolicitud)
 
-function realizarSolicitud(){
-    let tipo_vacio = input_tipo.value 
-    let filtro_vacio = input_filtro.value 
-    if(tipo_vacio == '' && filtro_vacio == ''){
-        fetch('https://cataas.com/cat?json=true')
-            .then(response => response.json())
-            .then(gato => {
-                console.log(gato)
-                imagen_respuesta.setAttribute('src',`https://cataas.com/${gato.url}`)
-                boton_editar.innerHTML = gato.id
-            })
-    }else if(tipo_vacio == '' && filtro_vacio != ''){
-        fetch(`https://cataas.com/cat?filter=${filtro_vacio}&json=true`)
-        .then(response => response.json())
-        .then(gato => {
-            console.log(gato)
-            imagen_respuesta.setAttribute('src',`https://cataas.com/${gato.url}`)
-        })
-    }else if(tipo_vacio != '' && filtro_vacio == ''){
-        fetch(`https://cataas.com/cat?type=${tipo_vacio}&json=true`)
-        .then(response => response.json())
-        .then(gato => {
-            console.log(gato)
-            imagen_respuesta.setAttribute('src',`https://cataas.com/${gato.url}`)
-        })
-    }else{
-        fetch(`https://cataas.com/cat?type=${tipo_vacio}&filter=${filtro_vacio}&json=true`)
-        .then(response => response.json())
-        .then(gato => {
-            console.log(gato)
-            imagen_respuesta.setAttribute('src',`https://cataas.com/${gato.url}`)
-        })
+function realizarSolicitud() {
+    let filtro_vacio = input_filtro.value
+    let alto_vacio = input_alto.value
+    let ancho_vacio = input_ancho.value
+
+    var endPoint = ""
+
+    if (alto_vacio == '' && ancho_vacio == '' && filtro_vacio == '') {
+        endPoint = `https://cataas.com/cat?json=true`;
+
+    } else if (alto_vacio != '' && ancho_vacio == '' && filtro_vacio == '') {
+        endPoint = `https://cataas.com/cat?height=${alto_vacio}&json=true`;        
+    } else if (alto_vacio == '' && ancho_vacio != '' && filtro_vacio == '') {
+        endPoint = `https://cataas.com/cat?width=${ancho_vacio}&json=true`; 
+    } else if (alto_vacio == '' && ancho_vacio == '' && filtro_vacio != '') {
+        endPoint = `https://cataas.com/cat?filter=${filtro_vacio}&json=true`; 
+    } else if (alto_vacio != '' && ancho_vacio != '' && filtro_vacio == '') {
+        endPoint = `https://cataas.com/cat?height=${alto_vacio}&width=${ancho_vacio}&json=true`; 
+    } else if (alto_vacio != '' && ancho_vacio == '' && filtro_vacio != '') {
+        endPoint = `https://cataas.com/cat?height=${alto_vacio}&filter=${filtro_vacio}&json=true`; 
+    } else if (alto_vacio == '' && ancho_vacio != '' && filtro_vacio != '') {
+        endPoint = `https://cataas.com/cat?width=${ancho_vacio}&filter=${filtro_vacio}&json=true`; 
+    } else {
+        endPoint = `https://cataas.com/cat?height=${alto_vacio}&width=${ancho_vacio}&filter=${filtro_vacio}&json=true`;         
     }
+
+
+    fetch(endPoint)
+        .then(response => response.json())
+        .then(gato => {
+            console.log(gato)
+            imagen_respuesta.setAttribute('src', `https://cataas.com/${gato.url}`)
+            boton_editar.innerHTML = gato.id
+        })
 }
 
